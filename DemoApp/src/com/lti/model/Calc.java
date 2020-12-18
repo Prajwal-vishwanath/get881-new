0ce85fade2e3033f34eb6c7ed22ccd52030686e4
Prajwal <Prajwal@LAPTOP-C5OH21RL>
2020-12-18 17:06:46 +0530

    ass

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -20,7 +20,7 @@ public class Calc {
 	public void mod(int num1, int num2) {
 		System.out.println("mod="+(num1%num2));
 	}
-  public void prod(int num1, int num2) {
+	public void prod(int num1, int num2) {
 		System.out.println("Product="+(num1*num2));
 	}
 	
