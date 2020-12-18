23c8ea91733f9327f8d4c50c780c31e044fc6859
Prajwal <Prajwal@LAPTOP-C5OH21RL>
2020-12-18 16:06:40 +0530

    first commit

---
+++ DemoApp/src/com/lti/model/Calc.java
@@ -0,0 +1,21 @@
+package com.lti.model;
+
+public class Calc {
+
+	public void add(int num1, int num2) {
+		System.out.println("Sum="+(num1+num2));
+	}
+	
+	public void sub(int num1, int num2) {
+		System.out.println("Difference="+(num1-num2));
+	}
+	
+	public void Mul(int num1, int num2) {
+		System.out.println("Multiplication="+(num1*num2));
+	}
+	public void div(int num1, int num2) {
+		System.out.println("Division="+(num1/num2));
+	}
+	
+	
+}
